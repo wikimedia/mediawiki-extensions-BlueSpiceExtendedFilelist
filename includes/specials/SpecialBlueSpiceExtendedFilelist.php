cb4375151980a7eee7fec17ec3a81a949c386ecf
pwirth <wirth@hallowelt.biz>
2018-10-18 09:21:02 +0200

    Remove usage of deprecated BsSpecialPage
    => needs cherry-pick to REL1_31
    
    Change-Id: Ib11cf08a36e7f2653801b85ce51c7ac341c46244

--- includes/specials/SpecialBlueSpiceExtendedFilelist.php
+++ includes/specials/SpecialBlueSpiceExtendedFilelist.php
@@ -1,6 +1,6 @@
 <?php
 
-class SpecialBlueSpiceExtendedFilelist extends BsSpecialPage {
+class SpecialBlueSpiceExtendedFilelist extends \BlueSpice\SpecialPage {
 	public function __construct(){
 		parent::__construct( 'BlueSpiceExtendedFilelist' );
 	}
