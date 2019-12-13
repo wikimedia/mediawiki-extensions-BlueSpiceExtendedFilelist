d2d3c8085cd38e81af14237b22fcd5214ef87765
pwirth <wirth@hallowelt.biz>
2019-12-13 15:47:38 +0100

    Fix global prefix and doc blocks for phpcs
    ERM:17071
    [3.1.x]
    
    Change-Id: Ia69a5ea6b96b6e74ff26556e0ebd760e13b24bda
    (cherry picked from commit 065e5af67ae09e57f77d3adc230c820b6b507bcc)

--- includes/specials/SpecialBlueSpiceExtendedFilelist.php
+++ includes/specials/SpecialBlueSpiceExtendedFilelist.php
@@ -5,18 +5,25 @@ class SpecialBlueSpiceExtendedFilelist extends \BlueSpice\SpecialPage {
 		parent::__construct( 'BlueSpiceExtendedFilelist' );
 	}
 
+	/**
+	 *
+	 * @param string $par
+	 */
 	public function execute( $par ) {
-		global $bsgEFLPluginModules;
-
 		parent::execute( $par );
 
 		$output = $this->getOutput();
 		$output->addModules( 'ext.bluespice.extendedFilelist' );
 		$output->addModuleStyles( 'ext.bluespice.extendedFilelist.styles' );
 		$output->addHTML( '<div id="bs-extendedfilelist-grid"></div>' );
-		$this->loadPluginModules( $output, $bsgEFLPluginModules );
+		$this->loadPluginModules( $output, $this->getConfig()->get( 'EFLPluginModules' ) );
 	}
 
+	/**
+	 *
+	 * @param OutputPage $output
+	 * @param array $aPluginModules
+	 */
 	private static function loadPluginModules( $output, $aPluginModules ) {
 		foreach ( $aPluginModules as $pluginModule ) {
 			$output->addModules( $pluginModule );
