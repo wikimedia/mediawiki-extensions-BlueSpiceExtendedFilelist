9fe4f120d167ceb9f76550e3f22dd8c9a64d1c5a
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-11 14:31:00 +0200

    Add extension to SkinSlot GlobalActionsTool
    [4.x]
    
    Change-Id: I5858f2c9f377728921ef73aed3220e41e92965cb

---
+++ src/GlobalActionsTool.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace BlueSpice\ExtendedFilelist;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsTool extends RestrictedTextLink {
+
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-extendedfilelist';
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getPermissions(): array {
+		return [ 'read' ];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHref(): string {
+		$tool = SpecialPage::getTitleFor( 'BlueSpiceExtendedFilelist' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bluespiceextendedfilelist' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-extendedfilelist-desc' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bluespiceextendedfilelist' );
+	}
+}
